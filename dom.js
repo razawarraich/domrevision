0aa0a79d61065d4c3081ed78fb8fe5e77dc190ca
razawarraich <muhammadraza5911g@gmail.com>
2026-08-29 00:03:45 +0500

    dompractice

---
+++ dom.js
@@ -0,0 +1,11 @@
+const headingg= document.querySelector('.heading1')
+// headingg.style.color="blue"
+
+const buttonn= document.querySelector('.button')
+buttonn.addEventListener('click', ()=>{
+    if (headingg.style.color==="blue"){
+        headingg.style.color="red"    
+    }else{
+        headingg.style.color="blue"
+    }
+});
