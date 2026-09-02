2194e44bf6c4a3e8282c570cc826d4b34e34f4b6
razawarraich <muhammadraza5911g@gmail.com>
2026-09-02 20:29:04 +0500

    dom

--- dom.js
+++ dom.js
@@ -19,4 +19,11 @@ buttonn.addEventListener('click', ()=>{
     }
 });
 
-
+const buttonn= document.querySelector('.button')
+buttonn.addEventListener('click', ()=>{
+    if (headingg.style.color==="blue"){
+        headingg.style.color="red"    
+    }else{
+        headingg.style.color="blue"
+    }
+});
